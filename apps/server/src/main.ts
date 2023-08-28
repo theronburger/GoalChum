bdefca70958e624bb9e403c6d76cf6b04a1b84f4
theronburger <dev@theronburger.com>
2023-08-28 23:09:43 +0200

    add serve static to server

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,14 +1,26 @@
-import koa from 'koa';
+'use strict';
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import serve from 'koa-static';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-const app = new koa();
+const app = new Koa();
+app.use(bodyParser());
 
-app.use(async (ctx) => {
-  ctx.body = { message: 'Hello API' };
+try {
+  process.stdout.write('🍯 Loading frontend. ');
+  app.use(serve('public'));
+  console.log('✅');
+} catch (error) {
+  console.log("Hmmm, that didn't work. Did you build it?");
+}
+
+app.use((ctx) => {
+  ctx.body = `🚧 Hit fallback\n\n${JSON.stringify(ctx, null, 2)}`;
 });
 
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+app.listen(port, () => {
+  console.log(`🦻 Listening for HTTP requests on http://${host}:${port}`);
 });
